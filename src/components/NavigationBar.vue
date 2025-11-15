2d7d7d048b7bfa42d6732b0f05af3426dcf47d99
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 19:28:38 +0000

    Update link path

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,7 +2,7 @@
   <nav class="desktop-nav">
     <ul>
       <li v-for="link in navLinks" :key="link.path">
-        <router-link :to="link.path" class="nav-item">{{ link.name }}</router-link>
+        <router-link :to="link.path">{{ link.name }}</router-link>
       </li>
     </ul>
   </nav>
