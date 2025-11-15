e00d953592feea0d5fa551821dd065fe728935ee
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 17:17:06 +0000

    Add SiteFooter component

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    <div>
+      <a href="mailto:laurenleighwoods@gmail.com" target="_blank" rel="noopener noreferrer">
+        <i class="fa-solid fa-envelope"></i>
+        info@mekanism.com
+      </a>
+    </div>
+    <ul class="social-icons">
+      <li>
+        <a href="https://x.com/aidancbrady" target="_blank" rel="noopener noreferrer">
+          <i class="fa-brands fa-x-twitter"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/aidancbrady" target="_blank" rel="noopener noreferrer">
+          <i class="fa-brands fa-github"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/aidancbrady/" target="_blank" rel="noopener noreferrer">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
