d4082d11d085d7bb4f4691d640607e5d6ad48603
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 17:37:00 +0000

    Add MobileNav component & placeholder code

---
+++ src/components/MobileNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="mobile-nav">
+    <button class="menu-btn" @click="toggleMenu" aria-label="Toggle menu">
+      <span :class="['bar', { open: isOpen }]"></span>
+      <span :class="['bar', { open: isOpen }]"></span>
+      <span :class="['bar', { open: isOpen }]"></span>
+    </button>
+    <Transition name="slide">
+      <ul v-if="isOpen" class="menu-list">
+        <li v-for="link in navLinks" :key="link.path">
+          <router-link :to="link.path" @click="closeMenu">{{ link.name }}</router-link>
+        </li>
+      </ul>
+    </Transition>
+  </nav>
+</template>
+<script setup>
+import { ref } from 'vue'
+import { navLinks } from '../navigation.js'
+const isOpen = ref(false)
+const toggleMenu = () => isOpen.value = !isOpen.value
+const closeMenu = () => isOpen.value = false
+</script>
+
+<style scoped>
+
+</style>
