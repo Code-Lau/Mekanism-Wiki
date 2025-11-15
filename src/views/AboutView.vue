4907aa49ec8e2eb21f6f080909886b6ebe9c0625
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 19:30:00 +0000

    Add placeholder img & styles

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,10 +7,8 @@
     <main>
       <section class="hero-section">
         <figure>
-          <video controls>
-            <source src="#" type="video/mp4">
+            <img src="../assets/images/variousmachines.webp" alt="A picture of various Mekanism machines">
             Your browser does not support the video tag
-          </video>
         </figure>
         <div class="hero-text">
           <h1>WHY USE MEKANISM?</h1>
@@ -28,5 +26,44 @@
 </template>
 
 <style scoped>
+h1 {
+  color: #162844;
+}
 
+h2 {
+  color: #162844;
+  margin:1em;
+}
+
+img {
+  width:100%;
+  border-radius: 2em;
+  object-fit:contain;
+  padding:8px;
+}
+
+
+.hero-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 100px;
+  margin-top:40px;
+}
+
+.hero-text p {
+  color: #162844;
+  font-size: 20px;
+  padding: 20px;
+}
+
+.hero-text {
+  padding: 12px;
+  color: #162844;
+  text-align: center;
+  line-height: 110%;
+  font-weight: 300;
+  word-spacing: 5px;
+  margin: 0 8px;
+}
 </style>
