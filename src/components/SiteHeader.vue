c3d9897b7984af1f3ceb515834f89ea4e287b28f
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 17:17:18 +0000

    Add SiteHeader component

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <a href="../../../index.html" class="logo">MEKANISM</a>
+    <div class="nav-menu">
+      <a href="">HOME</a>
+      <a href="">ABOUT</a>
+      <a href="">GUIDE</a>
+      <a href="">DOWNLOAD</a>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 10px 10px;
+  background-color: #162844;
+  position: relative;
+}
+
+.logo {
+  font-size: 64px;
+  font-weight: 700;
+  color: white;
+  cursor: pointer;
+}
+
+.nav-menu {
+  display: flex;
+  justify-content: space-between;
+  gap:15px;
+  margin-top:15px;
+  padding: 10px;
+  background-color: #162844;
+  position: relative;
+  font-weight: 700;
+  color:white;
+  font-size: 48px;
+}
+</style>
