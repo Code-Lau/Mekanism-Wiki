af7d63b771a49bbb449fb1114237313533eeb83b
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 17:38:36 +0000

    Add About content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,28 @@
 </script>
 
 <template>
-
+  <div class="home-wrapper">
+    <main>
+      <section class="hero-section">
+        <figure>
+          <video controls>
+            <source src="#" type="video/mp4">
+            Your browser does not support the video tag
+          </video>
+        </figure>
+        <div class="hero-text">
+          <h1>WHY USE MEKANISM?</h1>
+          <p>
+            Mekanism is all about automation and aims to make your resources worth more,
+            cut down on base game grinds and to offer a variety of personal tools.
+            Mekanism offers a jetpack, a robot companion, an everything-in-one super tool,
+            a meka suit and much more. If you would like a preview of whats to come, check out
+            the preview video. To learn how to make these tools and more, check out our guide section!
+          </p>
+        </div>
+      </section>
+    </main>
+  </div>
 </template>
 
 <style scoped>
