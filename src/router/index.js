4944632601931e57c5ede7b99bd0d5eb414d212b
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 19:43:23 +0000

    Add paths for new views

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+//Main Pages
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import GettingStartedView from "../views/GettingStartedView.vue";
+import DownloadsView from "../views/DownloadsView.vue";
+import ItemListView from "@/views/ItemListView.vue";
+
+//Items
+import OsmiumOreView from "@/views/Items/OsmiumOreView.vue";
 
 const routes = [
     { path: '/', name: 'Home', component: HomeView },
-    { path: '/about', name: 'About', component: AboutView }
+    { path: '/about', name: 'About', component: AboutView },
+    { path: '/gettingstarted', name: 'GettingStarted', component: GettingStartedView },
+    { path: '/downloads', name: 'Downloads', component: DownloadsView },
+    { path: '/itemlist', name: 'ItemList', component: ItemListView},
+    { path: '/osmiumore', name: 'OsmiumOre', component: OsmiumOreView }
 ]
 
 const router = createRouter({
