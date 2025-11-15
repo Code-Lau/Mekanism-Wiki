41c2c6816900a4020be284418eeda79b723c1e73
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 17:37:43 +0000

    Add placeholder structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
-<script setup>
-import HomePage from './components/HomePage.vue';
-</script>
-
 <template>
-  <div class="page-wrapper">
-    <HomePage />
+  <div>
+    <SiteHeader />
+    <NavigationBar />
+    <MobileNav />
+    <main>
+      <aside id="sidebarA">
+        <SidebarResources />
+      </aside>
+      <section>
+        <Transition name="fade" mode="out-in">
+          <router-view />
+        </Transition>
+      </section>
+      <aside id="sidebarB">
+        <SidebarPreparation />
+      </aside>
+    </main>
+    <SiteFooter />
   </div>
 </template>
 
+<script setup>
+import SiteHeader from './components/SiteHeader.vue'
+import SiteFooter from './components/SiteFooter.vue'
+</script>
+
 <style scoped>
-.page-wrapper {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
 </style>
