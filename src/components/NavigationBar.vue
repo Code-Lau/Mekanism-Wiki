bbaf52128d041484682e5579773cb70d7f90b363
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 17:22:51 +0000

    Add placeholder code

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="desktop-nav">
+    <ul>
+      <li v-for="link in navLinks" :key="link.path">
+        <router-link :to="link.path" class="nav-item">{{ link.name }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import { navLinks } from '../navigation.js'
+</script>
+
+<style scoped>
+
+</style>
