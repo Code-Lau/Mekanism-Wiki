8a56cf4f4186274a02b069a7c54bce5afa0d2257
Code-Lau <laurenleighwoods@gmail.com>
2025-11-15 19:43:28 +0000

    Add paths for new views

--- src/navigation.js
+++ src/navigation.js
@@ -1,4 +1,8 @@
 export const navLinks = [
     { name: 'Home', path: '/' },
-    { name: 'About', path: '/about' }
+    { name: 'About', path: '/about' },
+    { name: 'GettingStarted', path: '/gettingstarted' },
+    { name: 'Downloads', path: 'downloads' },
+    { name: 'ItemList', path: 'itemlist'},
+    { name: 'OsmiumOre', path: 'osmiumore'}
 ]
